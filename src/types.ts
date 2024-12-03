0539300fd26d2108fa281c5f415795502f45465a
Awad Khawaja <awadkhawaja@crc-dot1x-nat-10-239-158-37.bu.edu>
2024-12-03 17:47:20 -0500

    added racelist and race type

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Race = {
+  meeting_name: string;
+  meeting_official_name: string;
+  location: string;
+  country_key: number;
+  country_code: string;
+  country_name: string;
+  circuit_key: number;
+  circuit_short_name: string;
+  date_start: Date;
+  gmt_offset: Date;
+  meeting_key: number;
+  year: number;
+};
+  
