175ad381c641a7cae332c66d09c40992ecf55885
Awad Khawaja <awadkhawaja@crc-dot1x-nat-10-239-158-37.bu.edu>
2024-12-03 18:05:47 -0500

    quick updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter, Route, Routes, RouterProvider, renderMatches } from "react-router-dom";
+import AboutPage from "./components/about-page";
+import RaceList from "./components/RaceList";
 
 function Root() {
   return (
     <div>
       <Routes>
-        <Route path = "/" element = {<About/>}/>
+        <Route path = "/" element = {<AboutPage/>}/>
         <Route path = "/races" element = {<RaceList/>}/>
-        <Route path = "/race/:id" element = {</>}/>
-        <Route path = "/drivers" element = {<DriverList/>}/>
-        <Route path = "/driver/:id" element = {</>}/>
+        {/* <Route path = "/race/:id" element = {</>}/> */}
+        {/* <Route path = "/drivers" element = {<DriverList/>}/> */}
+        {/* <Route path="/driver/:id" element ={</>} /> */}
       </Routes>
     </div>
   );
