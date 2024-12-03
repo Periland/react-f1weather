ecf118b976b112ce483d9f09be669ae4331705ec
milozg <123111834+milozg@users.noreply.github.com>
2024-12-03 17:56:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,26 @@
+import { createBrowserRouter, Route, Routes, RouterProvider, renderMatches } from "react-router-dom";
 
+function Root() {
+  return (
+    <div>
+      <Routes>
+        <Route path = "/" element = {<About/>}/>
+        <Route path = "/races" element = {<RaceList/>}/>
+        <Route path = "/race/:id" element = {</>}/>
+        <Route path = "/drivers" element = {<DriverList/>}/>
+        <Route path = "/driver/:id" element = {</>}/>
+      </Routes>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([{path : "*", Component : Root}])
+
+function App() {
+
+  return (
+    <RouterProvider router = {router}/>
+  );
+}
+
+export default App
